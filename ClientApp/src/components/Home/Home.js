9ab80f80ce6ad27122e5bde22aad1ab3f01d534a
Chloe Jones <Chloe.Jones@softwire.com>
2021-04-21 09:55:05 +0100

    added interactive map, put borders on twitter widgets

--- ClientApp/src/components/Home/Home.js
+++ ClientApp/src/components/Home/Home.js
@@ -11,6 +11,11 @@ function Home() {
       <div className='weather-widget'>
         <iframe src='https://mars.nasa.gov/layout/embed/image/insightweather/' scrolling='no' frameborder='0'></iframe>
       </div>
+      <h2>Track a Rover</h2>
+      <p> Use the interactive map to track perserveances location since landing on Mars. </p>
+      <div className='roverMap'>
+        <iframe src='https://mars.nasa.gov/maps/location/?mission=M20&site=NOW' width='800' height='450' scrolling='no' frameborder='0' allowfullscreen></iframe>
+      </div>
     </div>
   )
 }
